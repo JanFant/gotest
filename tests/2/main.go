5eb2cdbe69e98f024419fb2bec8ea11703bb5746
JanFan <termal12345@gmail.com>
2019-08-28 15:50:48 +0600

    162 страница

--- tests/2/main.go
+++ tests/2/main.go
@@ -2,64 +2,14 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"unicode"
 )
 
-type statistics struct {
-	numbers []float64
-	mean    float64
-	median  float64
-	stdDev  float64
-	mode    []float64
-}
-
-func mode(stat statistics) []float64 {
-	var (
-		temp []float64
-	)
-	k := 1
-	pastK := 0
-	pastnNum := 0.0
-	sort.Float64s(stat.numbers)
-	for i, num := range stat.numbers {
-		if i == 0 {
-			pastnNum = num
-			k = 1
-			continue
-		} else {
-			if pastnNum == num {
-				k++
-			} else {
-				if pastK > k {
-					k = 1
-					pastnNum = num
-					continue
-				} else if pastK == k {
-					temp = append(temp, pastnNum)
-					k = 1
-				} else {
-					temp = temp[:0]
-					temp = append(temp, pastnNum)
-					pastK = k
-					k = 1
-				}
-			}
-			pastnNum = num
-		}
-	}
-	if pastK == k {
-		temp = append(temp, pastnNum)
-	} else if k > pastK {
-		temp = temp[:0]
-		temp = append(temp, pastnNum)
-	}
-
-	return temp
-}
-
 func main() {
-	var a statistics
-	a.numbers = append(a.numbers, 1, 2, 1, 1, 1, 2, 2, 1, 3, 2, 3, 3, 3, 3, 5, 5, 5, 5)
+	//162 c
+	fmt.Println(IsHexDigit('8'), IsHexDigit('x'), IsHexDigit('X'), IsHexDigit('b'), IsHexDigit('B'))
+}
 
-	fmt.Println(mode(a))
+func IsHexDigit(char rune) bool {
+	return unicode.Is(unicode.ASCII_Hex_Digit, char)
 }
