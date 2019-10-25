32cd70072fb653dccac0fcb8737476493b5481c4
JanFan <termal12345@gmail.com>
2019-10-25 12:24:32 +0600

    386

---
+++ tests/omap/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"./omap"
+)
+
+func main(){
+	omap.Prt()
+}
--- tests/omap/omap/omap.go
+++ tests/omap/omap/omap.go
@@ -1,1 +1,18 @@
 package omap
+
+type Map struct {
+	root *node
+	less func(interface{}, interface{}) bool
+	length int
+}
+
+type node struct {
+	key,value interface{}
+	red bool
+	left,right *node
+}
+
+func New(less func(interface{},interface{}) bool) *Map {
+	return
+}
+
